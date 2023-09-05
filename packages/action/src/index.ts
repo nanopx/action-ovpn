6db2dc38ca04f633e35f7d1968aea9f4c138be8a
nanopx <0nanopx@gmail.com>
2023-09-05 09:36:17 +0900

    Implement disconnect option

--- packages/action/src/index.ts
+++ packages/action/src/index.ts
@@ -1,10 +1,29 @@
 import * as core from '@actions/core'
 import { run as main } from './main'
-import { run as cleanup } from './post'
+import { run as post } from './post'
 
 const isPost = core.getState('isPost')
+const disconnect = core.getState('disconnect')
+const isCleanedUp = core.getState('isCleanedUp')
+
+async function cleanup() {
+  try {
+    const pid = core.getState('pid')
+    await post(pid)
+    core.saveState('isCleanedUp', 'true')
+  } catch (e) {
+    if (e instanceof Error) {
+      core.setFailed(e.message)
+    }
+  }
+}
 
 async function run() {
+  if (disconnect) {
+    await cleanup()
+    return
+  }
+
   if (!isPost) {
     core.saveState('isPost', 'true')
 
@@ -17,14 +36,12 @@ async function run() {
       }
     }
   } else {
-    try {
-      const pid = core.getState('pid')
-      await cleanup(pid)
-    } catch (e) {
-      if (e instanceof Error) {
-        core.setFailed(e.message)
-      }
+    if (isCleanedUp) {
+      core.info('VPN already disconnected.')
+      return
     }
+
+    await cleanup()
   }
 }
 
